0e263c3c77f8143d32350117eac60db7e1c9ddff
Sergey Levko <okvel.s@gmail.com>
2019-09-13 06:39:53 +0300

    FCC#004 - Update recycler view

---
+++ app/src/main/kotlin/by.liauko.siarhei.fcc/entity/Data.kt
@@ -0,0 +1,4 @@
+package by.liauko.siarhei.fcc.entity
+
+open class Data (val id: Long,
+                 var time: Long)
--- app/src/main/kotlin/by.liauko.siarhei.fcc/recyclerview/RecyclerViewDataAdapter.kt
+++ app/src/main/kotlin/by.liauko.siarhei.fcc/recyclerview/RecyclerViewDataAdapter.kt
@@ -8,23 +8,29 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import by.liauko.siarhei.fcc.R
+import by.liauko.siarhei.fcc.entity.Data
 import by.liauko.siarhei.fcc.entity.FuelConsumptionData
+import by.liauko.siarhei.fcc.entity.LogData
 import by.liauko.siarhei.fcc.util.DateConverter
 import java.util.Calendar
 
-class RecyclerViewDataAdapter(val dataSet: MutableList<FuelConsumptionData>,
+class RecyclerViewDataAdapter(val dataSet: ArrayList<Data>,
                               val resources: Resources,
                               val database: SQLiteDatabase,
                               private val listener: RecyclerViewOnItemClickListener)
-    : RecyclerView.Adapter<FuelConsumptionViewHolder>() {
+    : RecyclerView.Adapter<DataViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        FuelConsumptionViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false))
+        DataViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false))
 
-    override fun onBindViewHolder(holder: FuelConsumptionViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
         val item = dataSet[position]
-        holder.fuelConsumption.text = String.format("%.2f %s", item.fuelConsumption, resources.getString(R.string.consumption_value))
-        holder.parameters.text = String.format("%.2f %s / %.1f %s", item.litres, resources.getString(R.string.liters), item.distance, resources.getString(R.string.km))
+        if (item is LogData) {
+            bindLogItem(item, holder)
+        } else if (item is FuelConsumptionData) {
+            bindFuelItem(item, holder)
+        }
+
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = item.time
         holder.date.text = DateConverter.convert(calendar)
@@ -44,22 +50,32 @@ class RecyclerViewDataAdapter(val dataSet: MutableList<FuelConsumptionData>,
         notifyItemRangeChanged(position, dataSet.size)
     }
 
-    fun restoreItem(data: FuelConsumptionData, position: Int) {
+    fun restoreItem(data: Data, position: Int) {
         dataSet.add(position, data)
         notifyItemInserted(position)
+    }
+
+    private fun bindFuelItem(item: FuelConsumptionData, holder: DataViewHolder) {
+        holder.result.text = String.format("%.2f %s", item.fuelConsumption, resources.getString(R.string.consumption_value))
+        holder.parameters.text = String.format("%.2f %s / %.1f %s", item.litres, resources.getString(R.string.liters), item.distance, resources.getString(R.string.km))
+    }
+
+    private fun bindLogItem(item: LogData, holder: DataViewHolder) {
+        holder.result.text = item.title
+        holder.parameters.text = String.format("%d %s", item.mileage, resources.getString(R.string.km))
     }
 }
 
 interface RecyclerViewOnItemClickListener {
-    fun onItemClick(item: FuelConsumptionData)
+    fun onItemClick(item: Data)
 }
 
-class FuelConsumptionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    val fuelConsumption: TextView = itemView.findViewById(R.id.result) as TextView
+class DataViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val result: TextView = itemView.findViewById(R.id.result) as TextView
     val parameters: TextView = itemView.findViewById(R.id.parameters) as TextView
     val date: TextView = itemView.findViewById(R.id.date) as TextView
 
-    fun bind(item: FuelConsumptionData, listener: RecyclerViewOnItemClickListener) {
+    fun bind(item: Data, listener: RecyclerViewOnItemClickListener) {
         itemView.setOnClickListener { listener.onItemClick(item) }
     }
 }
