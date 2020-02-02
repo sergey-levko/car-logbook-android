6c3a309a37eaadd41f300b47a73a04c6ecfbfcb3
Sergey Levko <okvel.s@gmail.com>
2020-02-02 07:57:43 +0300

    CL-20 - Update app styles

--- app/src/main/kotlin/by.liauko.siarhei.fcc/util/ApplicationUtil.kt
+++ app/src/main/kotlin/by.liauko.siarhei.fcc/util/ApplicationUtil.kt
@@ -34,26 +34,6 @@ enum class AppTheme(val appId: Int, val dialogId: Int) {
     KITTY(
         R.style.AppDefault,
         R.style.DialogDefault
-    ),
-    BLUE(
-        R.style.AppBlue,
-        R.style.DialogBlue
-    ),
-    RED(
-        R.style.AppRed,
-        R.style.DialogRed
-    ),
-    ORANGE(
-        R.style.AppOrange,
-        R.style.DialogOrange
-    ),
-    GREEN(
-        R.style.AppGreen,
-        R.style.DialogGreen
-    ),
-    DARK(
-        R.style.AppDark,
-        R.style.DialogDark
     )
 }
 
