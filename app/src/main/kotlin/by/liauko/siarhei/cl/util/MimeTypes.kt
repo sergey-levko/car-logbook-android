b3e2135a42b9013b39f2fb2f549ac0deae4d325b
Sergey Levko <Okvel@users.noreply.github.com>
2020-08-31 07:30:00 +0300

    Release v4.1 (#47)
    * CL-36 - Replace export to excel file to export to pdf file (#44)
    
    * CL-36 - Replace export to excel file to export to pdf file
    
    * CL-36 - Move paint creating to separate method
    
    * CL-37 - Fix displaying export to pdf menu item on settings activities and update versions (#45)
    
    * CL-37 - Fix displaying export to pdf menu item on settings activities
    
    * CL-37 - Update Kotlin and dependencies versions
    
    * Set application version to 4.1

--- app/src/main/kotlin/by/liauko/siarhei/cl/util/MimeTypes.kt
+++ app/src/main/kotlin/by/liauko/siarhei/cl/util/MimeTypes.kt
@@ -4,5 +4,5 @@ object MimeTypes {
 
     const val TYPE_CLBDATA_FILE = "application/clbdata"
     const val TYPE_GOOGLE_DRIVE_FOLDER = "application/vnd.google-apps.folder"
-    const val TYPE_XLSX_FILE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    const val TYPE_PDF_FILE = "application/pdf"
 }
